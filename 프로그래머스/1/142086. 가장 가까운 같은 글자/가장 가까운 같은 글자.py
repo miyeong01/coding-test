dd9f2e96032389a86d8e566176e2e28de20ecfa6
miyeong01 <94594147+miyeong01@users.noreply.github.com>
2026-09-08 02:00:02 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 0.77 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.py
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.py
@@ -1,11 +1,16 @@
+# 문자열 s가 주어졌을 때, s이 각 위치마다 자신보다 앞에 나왔으면서,
+# 자신과 가장 가까운 곳에 있는 같은 글자가 몇 칸 앞에 있는가
+# 자신의 앞에 같은 글자가 없으면 -1 출력
+
 def solution(s):
-    answer = []
+    answer = [] # 각 문자열 별로 자신과 가장 가까운 곳에 있는 글자가 몇 칸 앞에 있는지
+    order = {} # 문자열 : 몇 번째에 나왔는지, 업데이트 됨
     
     for i in range(len(s)):
-        if s[i] in s[:i]:
-            all_index = list(filter(lambda x: s[x] == s[i], range(i)))
-            answer.append(i - all_index[-1])
+        if s[i] in order:
+            answer.append(i - order[s[i]])
         else:
             answer.append(-1)
+        order[s[i]] = i
         
     return answer
