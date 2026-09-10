b0eecd11c7dd4b005fed44e2f62924c1015645ce
miyeong01 <94594147+miyeong01@users.noreply.github.com>
2026-09-10 17:11:33 +0900

    [level 1] Title: 대충 만든 자판, Time: 0.62 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/1/160586. 대충 만든 자판/README.md
+++ 프로그래머스/1/160586. 대충 만든 자판/README.md
@@ -1,10 +1,10 @@
 # [level 1] 대충 만든 자판 - 160586 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/160586?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/160586) 
 
 ### 성능 요약
 
-메모리: 9.36 MB, 시간: 1.12 ms
+메모리: 11.5 MB, 시간: 0.62 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 01월 11일 18:59:18
+2026년 09월 10일 17:11:26
 
 ### 문제 설명
 
--- 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.py
+++ 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.py
@@ -1,21 +1,35 @@
+# 휴대폰 자판에는 하나의 키에 여러 개의 문자가 할당될 수 있음.
+# 키 하나에 여러 문자가 할당된 경우, 동일한 키를 연속해서 빠르게 누르면 할당된 순서대로 문자가 바뀜.
+# 휴대폰 자판은 키의 개수가 1~100개까지 있을 수 있음.
+# 특정 키를 눌렀을 때, 입력되는 문자들도 무작위로 배열
+# 같은 문자가 자판 전체에 여러 번 할당된 경우도 있음.
+# 키 하나에 같은 문자가 여러 번 할당된 경우도 있음.
+# 아예 할당되지 않은 경우도 존재
+# 이 휴대폰 자판으로 특정 문자열을 작성할 때, 키를 최소 몇 번 눌러야 그 문자열을 작성할 수 있는가
+# keymap : 1번 키부터 차례대로 할당된 문자들이 순서대로 담긴 문자열 배열
+# targets : 입력하려는 문자열들이 담긴 문자열 배열 
+# 목표 문자열을 작성할 수 없을 때는 -1 저장 
+
 def solution(keymap, targets):
-    min_press = {}
+    count = {} # 문자열 몇 번 눌러야되는지
     
-    for key in keymap:
-        for i, char in enumerate(key):
-            if char not in min_press or i + 1 < min_press[char]:
-                min_press[char] = i + 1
-    
-    answer = []
-    
-    for target in targets:
-        total = 0
-        for char in target:
-            if char in min_press:
-                total += min_press[char]
+    for k in keymap:
+        for i in range(len(k)):
+            if k[i] in count:
+                count[k[i]] = min(count[k[i]], i+1)
             else:
-                total = -1
+                count[k[i]] = i+1
+                
+    answer = []
+    for target in targets:
+        target_count = 0
+        for t in target:
+            if t not in count:
+                target_count = -1
                 break
-        answer.append(total)
+                
+            target_count += count[t]
+            
+        answer.append(target_count)
         
     return answer
