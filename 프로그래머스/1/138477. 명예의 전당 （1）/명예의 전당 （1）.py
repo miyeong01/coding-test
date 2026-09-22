ad727bf48c3a4d2db3621721da6285b2075f2bee
miyeong01 <94594147+miyeong01@users.noreply.github.com>
2026-09-22 15:41:07 +0900

    [level 1] Title: 명예의 전당 (1), Time: 0.43 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.py
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.py
@@ -1,17 +1,24 @@
+# 매일 출연한 가수의 점수가 지금까지 출연 가수들의 점수 중 상위 k번째 이내이면
+# 해당 가수의 점수를 명예의 전당이라는 목록에 올려 기념
+# 프로그램 시작 이후 초기 k일까지는 모든 출연 가수의 점수가 명예의 전당에 오르게 됨.
+# 출연 가수의 점수가 명예의 전당에 오르면 기존의 k번째 순위의 점수는 명예의 전당에서 내려오게 됨. 
+# k : 명예의 전당 목록의 점수의 개수
+# score : 1일부터 마지막 날까지 출연한 가수들의 점수
+# 매일 발표된 명예의 전당의 최하위 점수 출력
+
 def solution(k, score):
-    fame = [] # 명예의 전당
-    answer = [] # 발표 점수
-    for i in range(len(score)):
-        if len(fame) < k:
-            fame.append(score[i])
-            fame.sort()
-            answer.append(fame[0])
+    answer = [] # 매일 발표된 명예의 전당의 최하위 점수 
+    rank = [] # 명예의 전당
+    
+    for s in score:
+        if len(rank) < k:
+            rank.append(s)
         else:
-            if (score[i] >= max(fame)) or (max(fame) >= score[i] >= min(fame)):
-                fame.remove(min(fame))
-                fame.append(score[i])
-                fame.sort()
-                answer.append(fame[0])
-            else:
-                answer.append(fame[0])
+            if rank[-1] < s:
+                rank[-1] = s
+        rank.sort(reverse=True)
+        answer.append(rank[-1])
+        
     return answer
+        
+    
