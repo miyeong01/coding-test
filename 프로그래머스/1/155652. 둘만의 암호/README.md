7143c5bdf3bfb29ba22bbd7cdf1ee21164487432
miyeong01 <94594147+miyeong01@users.noreply.github.com>
2026-09-25 10:19:51 +0900

    [level 1] Title: 둘만의 암호, Time: 0.02 ms, Memory: 11.2 MB -BaekjoonHub

--- 프로그래머스/1/155652. 둘만의 암호/README.md
+++ 프로그래머스/1/155652. 둘만의 암호/README.md
@@ -1,10 +1,10 @@
 # [level 1] 둘만의 암호 - 155652 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/155652?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/155652) 
 
 ### 성능 요약
 
-메모리: 9.09 MB, 시간: 0.03 ms
+메모리: 11.2 MB, 시간: 0.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 01월 02일 17:00:38
+2026년 09월 25일 10:19:44
 
 ### 문제 설명
 
--- 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.py
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.py
@@ -1,16 +1,19 @@
+# 문자열 s의 각 알파벳을 index만큼 뒤의 알파벳으로 바꿔줌.
+# index만큼의 뒤의 알파벳이 z를 넘어갈 경우 다시 a로 돌아감.
+# skip에 있는 알파벳은 제외하고 건너뜀.
+
 def solution(s, skip, index):
+    alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
+               'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    for sk in skip:
+        alphabet.remove(sk)
+        
     answer = ''
     
-    alphabets = "abcdefghijklmnopqrstuvwxyz"
-    
-    for ch in skip:
-        alphabets = alphabets.replace(ch, "")
-    
-    for char in s:
-        curr_idx = alphabets.index(char)
-        
-        new_idx = (curr_idx + index) % len(alphabets)
-        
-        answer += alphabets[new_idx]
+    for a in s:
+        idx = alphabet.index(a)
+        answer += alphabet[(idx + index) % len(alphabet)]
         
     return answer
+        
+        
