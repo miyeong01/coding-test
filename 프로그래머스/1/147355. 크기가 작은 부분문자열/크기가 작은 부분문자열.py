0ec78c2e82ea5e8543526be93d7e751baff0a6b2
miyeong01 <94594147+miyeong01@users.noreply.github.com>
2026-09-26 10:24:16 +0900

    [level 1] Title: 크기가 작은 부분문자열, Time: 1.34 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.py
+++ 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.py
@@ -1,8 +1,14 @@
+# t, p : 숫자로 이루어진 문자열
+# t에서 p와 길이가 같은 부분문자열 중에서,
+# 이 부분문자열이 나타내는 수가 p가 나타내는 수보다 작거나 같은 것이 나오는 횟수 출력 
+
 def solution(t, p):
+    n = len(p)
     answer = 0
     
-    for i in range(len(t) - len(p) + 1):
-        if int(t[i:i+len(p)]) <= int(p):
+    for i in range(0, len(t)-n+1):
+        if int(t[i:i+n]) <= int(p):
             answer += 1
-            
+    
     return answer
+    
